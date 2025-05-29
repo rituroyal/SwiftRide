7ba5169b8aaf35dabbd85a3f9093977c968e6dfc
WishAccquire <vishakameena02@gmail.com>
2025-05-29 17:24:09 +0530

    run frontend and backend concurrently commit

--- Backend/controller/user.controoler.js
+++ Backend/controller/user.controoler.js
@@ -8,6 +8,7 @@ module.exports.registerUser=async(req,res,next)=>{
    if(!errors.isEmpty()){
     return res.status(400).json({errors:errors.array()});
    }
+   
    const { fullname, email, password } = req.body;
    
    const isUserExists = await User.findOne({ email });
